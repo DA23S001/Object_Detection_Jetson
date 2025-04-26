071de1d1db7af40b1048493e3f0e8afb66a941f4
DA23S001 <DA23S001@smail.iitm.ac.in>
2025-04-26 12:49:17 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name= 'ObjectDetectionJetson',
+    version='0.0.0',
+    author= 'Shashank',
+    author_email='DA23S001@smail.iitm.ac.in',
+    packages=find_packages(),
+    install_requires = []
+)
